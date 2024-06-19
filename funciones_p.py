8f82107e429438419849f09c4ec023ac03229997
ScarthPz <sca.perezm@duocuc.cl>
2024-06-19 10:24:03 -0400

    validaciones listas

--- funciones_p.py
+++ funciones_p.py
@@ -2,9 +2,9 @@ contactos = []
 
 def opcion_1():
     print("AGREGAR CONTACTO")
-    nombre = input("Ingrese nombre: ")
-    telefono = int(input("Ingrese teléfono: "))
-    correo = input("Ingrese correo: ")
+    nombre = validar_nombre
+    telefono = validar_telefono
+    correo = validar_correo
 
     contacto = {
         "nombre": nombre,
@@ -18,9 +18,7 @@ def opcion_1():
     
 
 def opcion_2():
-    if len(contactos) == 0:
-        print("no existe contactos, primero agregue alguno en la opción 1")
-    else:
+    if validar_contactos():
         print("LISTA DE CONTACTOS")
         for c in contactos:
             print(f"NOMBRE: {c['nombre']}")
@@ -28,9 +26,7 @@ def opcion_2():
             print(f"CORREO: {c['correo']}\n")
 
 def opcion_3():
-    if len(contactos) == 0:
-        print("no existen contactos, primero agregue alguno en la opción 1!")
-    else:
+   if validar_contactos():
         nombre_archivo = input("Ingrese nombre del archivo: ")
         import csv
         with open(nombre_archivo+".csv", "w", newline="") as archivo:
@@ -43,3 +39,61 @@ def opcion_3():
 def opcion_4():
     print("Adios!")
     exit()
+
+
+
+
+
+############
+    #validacion
+
+def validar_opciones(lista_opciones):
+    while True:
+        try:
+            opc = int(input("Ingrese opción: "))
+            if opc in lista_opciones:
+                return opc
+            else:
+                print("ERROR! opción incorrecta ")
+        except:
+            print("ERROR! debe ingresar un número entero!")
+
+
+def validar_nombre():
+    while True:
+        nombre = input("Ingrese nombre: ")
+        if len(nombre)>3 and nombre.isalpha():
+            return nombre
+        else:
+            print("ERROR! el nombre debe tener al menos 3 letras ")
+
+
+def validar_telefono():
+    while True:
+        try:
+            telefono = int(input("Ingrese teléfono: "))
+            if len(str(telefono)) == 9 and str(telefono)[0]=='9':
+                return telefono
+            else:
+                print("ERROR! el teléfono debe comenzar con 9 y tener 9 digitos!")
+
+        except:
+            print("ERROR! debe ingresar un número entero!")
+
+
+def validar_correo():
+    while True:
+        correo = input("Ingrese correo: ")
+        if correo.lower().endswith("@gmail.com") and len(correo.strip())>=13:
+            return correo
+        else:
+            print("ERROR! correo incorrecto")
+#pattern ---> buscar
+            
+
+def validar_contactos():
+    if len(contactos) == 0:
+        print("no existen contactos, agregue usando la opción 1!")
+        return False
+    return True
+
--- practicaprueba.py
+++ practicaprueba.py
@@ -1,10 +1,15 @@
+import os, time
 from funciones_p import *
 
 
 while True:
+    os.system("cls")
     print("MENÚ CONTACTOS")
     print("1. Agregar contacto\n2. Mostrar contactos\n3. Guardar archivo CSV\n4. Salir")
+
     opc = int(input("Ingrese opción: "))
+
+    os.system("cls")
     if opc == 1:
         opcion_1()
     elif opc == 2:
@@ -12,4 +17,6 @@ while True:
     elif opc == 3:
         opcion_3()
     else:
-        opcion_4 
+        opcion_4
+    time.sleep(3)
+
